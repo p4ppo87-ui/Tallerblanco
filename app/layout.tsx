9748f450efe64b0c13513b5df2441e6d50e1da42
p4ppo87-ui <p4ppo87@gmail.com>
2026-04-12 08:34:11 -0300

    fix viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </head>
       <body style={{ margin: 0, padding: 0 }}>
         {children}
       </body>
